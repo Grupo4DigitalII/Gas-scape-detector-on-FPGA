62ded0647d07d994c473d79eec5cd761105fd7c8
Grupo4DigitalII <108437348+Grupo4DigitalII@users.noreply.github.com>
2022-07-01 08:27:01 -0500

    Update esp32_proj.ino

--- codigosArduinoIDE/esp32_proj/esp32_proj.ino
+++ codigosArduinoIDE/esp32_proj/esp32_proj.ino
@@ -1,6 +1,6 @@
-#define BLYNK_TEMPLATE_ID "TMPL-uBWhYEV"
-#define BLYNK_DEVICE_NAME "ESP32"
-#define BLYNK_AUTH_TOKEN "Zkcns-OCFZheCG5vnHrQPTrl498yX6ay"
+#define BLYNK_TEMPLATE_ID "************"                      //Credenciales de Blynk.
+#define BLYNK_DEVICE_NAME "*****"                             //Deben estar al inicio.
+#define BLYNK_AUTH_TOKEN "*******************************"
 #define BLYNK_PRINT Serial
 
 #include <WiFi.h>
@@ -9,8 +9,8 @@
 #include <Blynk.h>
 #include <SoftwareSerial.h>
 
-#define WIFI_SSID "Redmi Note 10Sc"
-#define WIFI_PASSWORD "Sierra117"
+#define WIFI_SSID "*******"
+#define WIFI_PASSWORD "********"
 
 SoftwareSerial fpga(16,17);
 
@@ -29,24 +29,24 @@ void setup() {
 
 void loop() {
 
-  if(fpga.read() == 1){
-    Serial.println("Wait...");
-    while(i<3){
-      while(DATO == -1){
+  if(fpga.read() == 1){                                      //Se espera el aviso de la FPGA
+    Serial.println("Wait...");                               //sobre el envío de datos.
+    while(i<3){                                              //Se reciben dos partes de dato.
+      while(DATO == -1){                                     // Se espera el envío de cada uno.
         DATO = fpga.read();
         //Serial.print(".");
       }
       if(i == 1){
-        DATOF += (DATO*100);
+        DATOF += (DATO*100);                                 //Digitos más significativos.
       }
       else if (i == 2){
-        DATOF += DATO;
+        DATOF += DATO;                                       //Digitos menos significativos.
       }
       i++;
       DATO = -1;
     }
     Blynk.run();
-    Blynk.virtualWrite(V0, DATOF);
+    Blynk.virtualWrite(V0, DATOF);                          //Envío de datos a la plataforma.
     Serial.print("Dato FPGA: ");
     Serial.println(DATOF);
     }
